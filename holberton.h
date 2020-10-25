d3f4e2cf307dd2e776bb25d734e7827086393fdd
ArtisanGray <1697@holbertonschool.com>
2020-10-25 03:29:36 +0000

    initial test commit to the remote branch

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+int _printf(const char *format, ...);
+int _putchar(va_list(args));
+int _puts(va_list(args));
+int _itoa(va_list(args));
+int _ftoa(va_list(args));
+
+#endif
