28dae2aed32e3a713e052aa23832d1a872f09316
Derek_Clemens <urwithinrange@yahoo.com>
2020-10-27 19:37:35 +0000

    Added functions to _puts

--- _puts.c
+++ _puts.c
@@ -1,5 +1,40 @@
 #include "holberton.h"
 /**
+ *_strlen - gives the length of a array using a loop
+ *@s: is the value we are given
+ *
+ *Return: n the desired length of our string
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
+/**
+ *_print_rev_recursion - function that prints a string in reverse
+ *@s: the string
+ *
+ *
+ *Return: void
+ */
+int _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return(_strlen(s));
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+/**
  * _puts - Function that prints a string
  * @args: The pointer that leads to the string
  *
